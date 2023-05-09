933e0ef17d63389a1edf4dba7e1d4f4709266ce0
Gustavo Menezes <gustavo.menezes@live.com>
2023-05-09 12:51:51 -0300

    atualizando arquivo app.java

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ola, Mundo!");
+        System.out.println("Olaaaa, meu povo bonitao da poxa!");
     }
 }
