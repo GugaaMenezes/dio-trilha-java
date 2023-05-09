780ee5bfc41bece8fdb9c9dd882f7717d7c22486
Gustavo Menezes <gustavo.menezes@live.com>
2023-05-09 16:32:31 -0300

    Criando o projeto para exemplo de incremento, decremente e inversao

---
+++ tipos-variaveis/src/OperadoresIncreDecre.java
@@ -0,0 +1,32 @@
+public class OperadoresIncreDecre {
+    public static void main(String[] args) {
+        
+        int numero = 5;
+        // x repeticao
+
+        //numero = numero + 1;
+
+        System.out.println(++ numero);
+
+        System.out.println(numero);
+
+
+
+
+        // decremento
+
+        //numero = numero - 1;
+        System.out.println(-- numero);
+        
+        System.out.println(numero);
+
+
+        boolean variavel = true;
+
+        System.out.println(!variavel);
+
+        variavel = !variavel;
+
+        System.out.println(!variavel);
+    }
+}
