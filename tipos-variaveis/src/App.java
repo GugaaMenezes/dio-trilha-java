d1ea6aaa1e50067ace12e41b46c71e52609c4788
GugaaMenezes <109118782+GugaaMenezes@users.noreply.github.com>
2023-05-09 12:52:50 -0300

    Update App.java

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olaaaa, meu povo bonitao da poxa!");
+        System.out.println("testando arquivo de volta");
     }
 }
