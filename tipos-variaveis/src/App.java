089650cfb9913c5d70caecba4061dc756463079d
Gustavo Menezes <gustavo.menezes@live.com>
2023-05-09 12:57:11 -0300

    atualizando arquivo ida app.java

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("testando arquivo de volta");
+        System.out.println("testando arquivo de idaa");
     }
 }
